ea069d5f4213891a98abea21ec18c77aab59d7c9
Francesco Montanaro <56433128+FrancescoMontanaro@users.noreply.github.com>
2021-05-05 11:38:57 +0200

    data augmentation

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
 import tensorflow as tf
+from PIL import Image
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import pandas as pd
+
+
+# Global Variables
+bs = 8
+num_classes = 10
+img_h = 28
+img_w = 28
+apply_data_augmentation = True
+
+SEED = 1234
+tf.random.set_seed(SEED)
 
 # Data Loading
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
 
-print(len(x_train))
+
+# Data augmentation
+if apply_data_augmentation:
+  train_data_gen = ImageDataGenerator(width_shift_range=10, height_shift_range=10, zoom_range=0.1, horizontal_flip=True, fill_mode='nearest', rescale=1./255)
+else:
+  train_data_gen = ImageDataGenerator(rescale=1./255)
+
+valid_data_gen = ImageDataGenerator(rescale=1./255)
+test_data_gen = ImageDataGenerator(rescale=1./255)
